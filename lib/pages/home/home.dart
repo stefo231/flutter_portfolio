6ecb0f69343e44a74d0e6eb6952c00d13e05d29d
Stefan Zdravkov <szdravkov93@gmail.com>
2021-06-02 11:33:07 +0200

    header

---
+++ lib/pages/home/home.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_portfolio/utils/constants.dart';
+import 'package:responsive_portfolio/utils/globals.dart';
+
+import 'components/header.dart';
+
+class Home extends StatelessWidget {
+  // const Home({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: Globals.scaffoldKey,
+      endDrawer: Drawer(
+        child: SafeArea(
+            child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 24.0),
+          child: ListView.separated(
+            itemBuilder: (BuildContext context, int index) {
+              return headerItems[index].isButton
+                  ? MouseRegion(
+                      cursor: SystemMouseCursors.click,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: kDangerColor,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        padding: EdgeInsets.symmetric(horizontal: 28.0),
+                        child: TextButton(
+                          onPressed: headerItems[index].onTap,
+                          child: Text(
+                            headerItems[index].title,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 13.0,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    )
+                  : ListTile(
+                      title: Text(
+                        headerItems[index].title,
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 13.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    );
+            },
+            separatorBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                height: 10.0,
+              );
+            },
+            itemCount: headerItems.length,
+          ),
+        )),
+      ),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Header(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
